32b6438c3799e50d593cdc54e8f0b51fad3038ca
KOLDERWH <kolderwh@gmail.com>
2023-12-18 18:31:35 +0800

    feat: add waline db structures

---
+++ waline.sql
@@ -0,0 +1,92 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+SET NAMES utf8mb4;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table wl_Comment
+# ------------------------------------------------------------
+
+CREATE TABLE `wl_Comment` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `comment` text,
+  `insertedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `ip` varchar(100) DEFAULT '',
+  `link` varchar(255) DEFAULT NULL,
+  `mail` varchar(255) DEFAULT NULL,
+  `nick` varchar(255) DEFAULT NULL,
+  `pid` int(11) DEFAULT NULL,
+  `rid` int(11) DEFAULT NULL,
+  `sticky` boolean DEFAULT NULL,
+  `status` varchar(50) NOT NULL DEFAULT '',
+  `like` int(11) DEFAULT NULL,
+  `ua` text,
+  `url` varchar(255) DEFAULT NULL,
+  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+# Dump of table wl_Counter
+# ------------------------------------------------------------
+
+CREATE TABLE `wl_Counter` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `time` int(11) DEFAULT NULL,
+  `reaction0` int(11) DEFAULT NULL,
+  `reaction1` int(11) DEFAULT NULL,
+  `reaction2` int(11) DEFAULT NULL,
+  `reaction3` int(11) DEFAULT NULL,
+  `reaction4` int(11) DEFAULT NULL,
+  `reaction5` int(11) DEFAULT NULL,
+  `reaction6` int(11) DEFAULT NULL,
+  `reaction7` int(11) DEFAULT NULL,
+  `reaction8` int(11) DEFAULT NULL,
+  `url` varchar(255) NOT NULL DEFAULT '',
+  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+# Dump of table wl_Users
+# ------------------------------------------------------------
+
+CREATE TABLE `wl_Users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `display_name` varchar(255) NOT NULL DEFAULT '',
+  `email` varchar(255) NOT NULL DEFAULT '',
+  `password` varchar(255) NOT NULL DEFAULT '',
+  `type` varchar(50) NOT NULL DEFAULT '',
+  `label` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `github` varchar(255) DEFAULT NULL,
+  `twitter` varchar(255) DEFAULT NULL,
+  `facebook` varchar(255) DEFAULT NULL,
+  `google` varchar(255) DEFAULT NULL,
+  `weibo` varchar(255) DEFAULT NULL,
+  `qq` varchar(255) DEFAULT NULL,
+  `2fa` varchar(32) DEFAULT NULL,
+  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
